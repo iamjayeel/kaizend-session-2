381dd508249b3529467d746a64fbe4e1a5ac096f
Jay Tero <iamjayeel@gmail.com>
2019-09-21 15:18:01 +0800

    Add exercises

---
+++ exercise7.py
@@ -0,0 +1,25 @@
+# Challenge - Classes Exercise
+
+# Add a method to the Car class called age
+# that returns how old the car is (2019 - year)
+
+# *Be sure to return the age, not print it
+
+# class Car:
+
+#     def __init__(self,year, make, model):
+#         self.year = year
+#         self.make = make
+#         self.model = model
+
+class car:
+
+	def __init__(self, year, make, model):
+		self.year = year
+		self.make = make
+		self.model = model
+
+	def age():
+		return 2019 - self.year
+
+Car = car(2001, "Limo", "Limo")
